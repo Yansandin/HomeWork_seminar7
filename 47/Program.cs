7ed36eee80bb058bce2dfc1bcd4ab8f456b289ef
San <yan_san_din@mail.ru>
2023-02-10 10:43:55 +0300

    added 47

---
+++ 47/Program.cs
@@ -0,0 +1,31 @@
+﻿double[,] GenerateArray(int m, int n, int min, int max)
+{
+    double[,] array = new double[m, n];
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j]=random.NextDouble()*10-10;
+            
+        }
+        
+    }
+    return array;
+}
+
+void PrintArray(double [,] array)
+{
+     for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j]+ " ");
+            
+        }
+        Console.WriteLine();
+    }
+}
+
+var array = GenerateArray(3,4, -10, 10);
+PrintArray(array);
